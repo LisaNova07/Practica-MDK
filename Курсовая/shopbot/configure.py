91bddca30c75c7234e614efe6be24d180b947c7f
LisaNova07 <gerlin05@mail.ru>
2022-02-22 07:10:20 +0300

    Курсовая

--- Курсовая/shopbot/configure.py
+++
@@ -1,6 +0,0 @@
-config = {
-	'name': 'mrbizon_shopbot',
-	'token': '5133356508:AAG1Uo7hMe4UNNGp9T-wnRgAxIGDiwA4rVw',
-	'tokenqiwi': 'a262658ab896fcf9fc5e916deead2c8c',
-	'phoneqiwi': '+79112256289'
-}
